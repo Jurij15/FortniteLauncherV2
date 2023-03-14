ed0720833edb4298d78f50c50287fb3b0c9fc28b
Jurij15 <jurij.grom1@gmail.com>
2023-03-14 19:02:40 +0100

    added psychopast's proxy (will use unless i deciede to make my own one from scratch)

---
+++ LawinServer/Core/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Win32;
+using System.Collections.Generic;
+
+namespace LawinServer.Core
+{
+    internal static class Extensions
+    {
+        public static void SetValues(this RegistryKey registryKey, List<RegistryInfo> registryInfos) => registryInfos
+                .ForEach(x => registryKey
+                .SetValue(x.Name, x.Value, x.RegistryValueKind));
+    }
+}
