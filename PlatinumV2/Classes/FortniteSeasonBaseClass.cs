d973ac8eccc599d3291033eca4adb0ca4fdefb24
Jure Jurij Grom <79604050+Jurij15@users.noreply.github.com>
2024-01-23 15:35:52 +0100

    rewrite started

---
+++ PlatinumV2/Classes/FortniteSeasonBaseClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlatinumV2.Classes
+{
+    public class FortniteSeasonBaseClass
+    {
+        public string SeasonName {  get; set; }
+
+        public List<string> BuildNames { get; set; }
+
+        public string SeasonDisplayName { get; set; }
+        public string SeasonDescription { get; set; }
+    }
+}
